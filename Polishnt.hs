5acb05f4017b630b7bb521afc4a30fcda8d96371
Aria Edmonds <aria@ar1as.space>
2022-05-18 17:28:30 +0800

    use haskeline instead of readline

--- Polishnt.hs
+++ Polishnt.hs
@@ -2,42 +2,44 @@
 
 module Polishnt where
 
-import Data.Number.CReal
-import qualified Data.Text as T
-import System.Console.ANSI
-import System.Console.Readline
-import System.IO
-import Text.Read
+import           Control.Monad.IO.Class         ( liftIO )
+import           Data.Number.CReal
+import qualified Data.Text                     as T
+import           System.Console.ANSI
+import           System.Console.Haskeline
+import           System.IO
+import           Text.Read
 
 type Stack = [Rational]
 
 run :: Stack -> IO ()
-run stack = do
-  maybeLine <- readline "% "
-  case maybeLine of
-    Nothing -> pure ()
-    Just ":q" -> pure ()
-    Just "quit" -> pure ()
-    Just input -> do
-      addHistory input
-      case input of
-        "discard" -> discard stack
-        "sqrt" -> sqrt' stack
-        "e" -> const' stack $ exp 1
-        "pi" -> const' stack pi
-        c
-          | c `elem` ["xy", "swap"] -> xy stack
-          | c `elem` ["+", "-", "*", "/", "^", "log"] ->
-            doOperation stack $ T.pack input
-        _ -> addNumber stack $ readInput $ T.pack input
+run stack = runInputT defaultSettings loop
+ where
+  loop :: InputT IO ()
+  loop = do
+    maybeLine <- getInputLine "% "
+    case maybeLine of
+      Nothing     -> pure ()
+      Just ":q"   -> pure ()
+      Just "quit" -> pure ()
+      Just input  -> liftIO $ do
+        case input of
+          "discard" -> discard stack
+          "sqrt"    -> sqrt' stack
+          "e"       -> const' stack $ exp 1
+          "pi"      -> const' stack pi
+          c
+            | c `elem` ["xy", "swap"] -> xy stack
+            | c `elem` ["+", "-", "*", "/", "^", "log"] -> doOperation stack
+            $  T.pack input
+          _ -> addNumber stack $ readInput $ T.pack input
 
 readInput :: T.Text -> Maybe Double
-readInput s
-  | "." `T.isPrefixOf` s = readInput $ '0' `T.cons` s
-  | otherwise = readMaybe $ T.unpack s
+readInput s | "." `T.isPrefixOf` s = readInput $ '0' `T.cons` s
+            | otherwise            = readMaybe $ T.unpack s
 
 doOperation :: Stack -> T.Text -> IO ()
-doOperation stack@(_:_:_) input = do
+doOperation stack@(_ : _ : _) input = do
   let stack' = perform stack input
   -- move up 1 line for the enter you just pressed and 2 lines for the 2
   -- numbers we're popping
@@ -62,23 +64,22 @@ addNumberStack :: Stack -> Double -> Stack
 addNumberStack stack n = toRational n : stack
 
 perform :: Stack -> T.Text -> Stack
-perform (y:x:s) op =
-  case op of
-    "+" -> x + y : s
-    "-" -> x - y : s
-    "*" -> x * y : s
-    "/" -> x / y : s
-    "log" ->
-      let x' = realToFrac x :: Double
-          y' = realToFrac y :: Double
-          result = logBase y' x'
-       in toRational result : s
-    "^" ->
-      let x' = realToFrac x :: Double
-          y' = realToFrac y :: Double
-          result = x' ** y'
-       in toRational result : s
-    _ -> error "you should never see this"
+perform (y : x : s) op = case op of
+  "+" -> x + y : s
+  "-" -> x - y : s
+  "*" -> x * y : s
+  "/" -> x / y : s
+  "log" ->
+    let x'     = realToFrac x :: Double
+        y'     = realToFrac y :: Double
+        result = logBase y' x'
+    in  toRational result : s
+  "^" ->
+    let x'     = realToFrac x :: Double
+        y'     = realToFrac y :: Double
+        result = x' ** y'
+    in  toRational result : s
+  _ -> error "you should never see this"
 perform _ _ = error "you should never see this 2"
 
 discard :: Stack -> IO ()
@@ -92,8 +93,8 @@ discard stack = do
   run stack'
 
 discardElement :: Stack -> Stack
-discardElement [] = []
-discardElement (_:xs) = xs
+discardElement []       = []
+discardElement (_ : xs) = xs
 
 sqrt' :: Stack -> IO ()
 sqrt' [] = do
@@ -106,11 +107,11 @@ sqrt' xs = do
   run stack
 
 sqrtStack :: Stack -> Stack
-sqrtStack [] = []
-sqrtStack (x:xs) = toRational (sqrt (fromRational x :: Double)) : xs
+sqrtStack []       = []
+sqrtStack (x : xs) = toRational (sqrt (fromRational x :: Double)) : xs
 
 xy :: Stack -> IO ()
-xy stack@(_:_:_) = do
+xy stack@(_ : _ : _) = do
   let stack' = xyStack stack
   upAndClear 3
   putStrLn $ printRational (stack' !! 1)
@@ -121,8 +122,8 @@ xy xs = do
   run xs
 
 xyStack :: Stack -> Stack
-xyStack (x:y:xs) = y : x : xs
-xyStack xs = xs
+xyStack (x : y : xs) = y : x : xs
+xyStack xs           = xs
 
 const' :: Stack -> Double -> IO ()
 const' xs x = do
